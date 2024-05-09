258d655cf008ae1215c8ccf6a8465ddd2af0208a
Camila Castro <cami@Camilas-MacBook-Air.local>
2024-05-09 18:27:44 -0300

    Validar mejor email

--- back/src/middlewares/User/validateEmail.ts
+++ back/src/middlewares/User/validateEmail.ts
@@ -1,12 +1,18 @@
 import { NextFunction, Request, Response } from "express"
 import CustomError from "../../errors/CustomError"
+import { getUsersService } from "../../services/usersServices"
 
-const validateEmail = (req: Request,res: Response,next: NextFunction) => {
+const validateEmail = async (req: Request,res: Response,next: NextFunction) => {
     const {email} = req.body
     const regexMail = /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/
 
     if (!regexMail.test(email)) {
         const err = new CustomError("Incorrect email",400)
+            next(err)
+    }
+    const emails = await getUsersService()
+    if(emails.includes(email)){
+        const err = new CustomError("This mail is already in use",400)
             next(err)
     } else next()
 }
