1c0dcc67a21dcf00ca834b90e9bbee59f9071e89
Camila Castro <cami@Camilas-MacBook-Air.local>
2024-05-13 00:16:29 -0300

    Mejorar estética de navbar

--- front/vite-project/src/components/Navbar/Navbar.jsx
+++ front/vite-project/src/components/Navbar/Navbar.jsx
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom"
 const Navbar = () => {
     return (
         <nav>
+            <Link to="/"><img className={styles.logo} src="../src/assets/logo.png" alt="logo" /></Link>
             <div className={styles.navbarLinks}>
-                <Link to="/"><img className={styles.logo} src="../src/assets/logo.png" alt="logo" /></Link>
                 <Link to="/about">About Us</Link>
                 <Link to="/contact">Contact Us</Link>
+                <Link to="/schedule">My Schedule</Link>
                 <Link to="/login">Login</Link>
                 <Link to="/register">Sign Up</Link>
-                <Link to="/schedule">My Schedule</Link>
             </div>
         </nav>
     )
