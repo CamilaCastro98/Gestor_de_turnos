a9216fa785e93fe413c3162e1a3db784893e5626
Camila Castro <cami@Camilas-MacBook-Air.local>
2024-08-05 17:23:08 -0300

    Config de vite

--- front/vite-project/vite.config.js
+++ front/vite-project/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    // Configuración para el directorio público
+    // Asegúrate de que 'public' esté correctamente especificado
+    // Por defecto, Vite incluye la carpeta 'public' en el directorio de salida
+  },
+  // Si tienes un directorio público distinto o personalizado, especifícalo aquí
+  publicDir: 'public', // Asegúrate de que esta carpeta esté incluida
+});
